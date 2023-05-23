80def79dbc478453caa0dd9bf4103ba40e7f173a
Karol Wilk <karolwilk@pop-os.localdomain>
2023-05-23 12:39:19 +0200

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
-use bsp::entry;
+use bsp::{
+    entry,
+    hal::{self, Adc, Clock},
+};
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::prelude::_embedded_hal_adc_OneShot;
+use heapless::String;
 use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
@@ -15,20 +16,55 @@ use panic_probe as _;
 use rp_pico as bsp;
 // use sparkfun_pro_micro_rp2040 as bsp;
 
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
+use bsp::hal::{clocks::init_clocks_and_plls, pac, sio::Sio, watchdog::Watchdog};
+use usb_device::{
+    class_prelude::{InterfaceNumber, UsbBus, UsbBusAllocator, UsbClass},
+    endpoint::{Endpoint, EndpointDirection, EndpointIn, In, Out},
+    prelude::{UsbDeviceBuilder, UsbVidPid},
+    UsbDirection, UsbError,
 };
+use usbd_serial::SerialPort;
+
+use core::fmt::Write;
+
+const BATCH_SIZE: usize = 32;
+
+struct BulkClass<'a, B: UsbBus> {
+    ep_in: EndpointIn<'a, B>,
+    iface: InterfaceNumber,
+}
+
+impl<'a, B: UsbBus> BulkClass<'a, B> {
+    pub fn new(alloc: &'a UsbBusAllocator<B>) -> Self {
+        Self {
+            ep_in: alloc.bulk(64),
+            iface: alloc.interface(),
+        }
+    }
+
+    pub fn write(&self, bytes: &[u8]) -> Result<usize, UsbError> {
+        self.ep_in.write(bytes)
+    }
+}
+
+impl<'a, B: UsbBus> UsbClass<B> for BulkClass<'a, B> {
+    fn get_configuration_descriptors(
+        &self,
+        writer: &mut usb_device::descriptor::DescriptorWriter,
+    ) -> usb_device::Result<()> {
+        writer.interface(self.iface, 0xff, 0x00, 0x00)?;
+        writer.endpoint(&self.ep_in)?;
+
+        Ok(())
+    }
+}
 
 #[entry]
 fn main() -> ! {
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
+    let mut adc = pac.ADC;
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
@@ -44,29 +80,66 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
-    let pins = bsp::Pins::new(
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
-    // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here.
-    let mut led_pin = pins.led.into_push_pull_output();
+    let usb_bus = hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    );
+
+    // Set up the USB driver
+    let usb_bus_allocator = UsbBusAllocator::new(usb_bus);
+
+    let mut bulk = BulkClass::new(&usb_bus_allocator);
+
+    // Set up the USB Communications Class Device driver
+    let mut serial = SerialPort::new(&usb_bus_allocator);
+
+    // Create a USB device with a fake VID and PID
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus_allocator, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .build();
+
+    unsafe {
+        adc.fcs.write_with_zero(|w| w.en().set_bit());
+        adc.cs.write_with_zero(|w| w.start_many().set_bit());
+    }
+
+    let gpio26 = pins.gpio26.into_floating_input();
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        usb_dev.poll(&mut [&mut bulk, &mut serial]);
+
+        let mut measures = [0 as u8; BATCH_SIZE * 2];
+
+        for i in 0..BATCH_SIZE {
+            while adc.fcs.read().empty().bit() {}
+            let counts: u16 = adc.fifo.read().val().bits();
+            let first_byte_index = i * 2;
+            let second_byte_index = first_byte_index + 1;
+
+            measures[first_byte_index] = (counts >> 8).try_into().unwrap_or(255);
+            measures[second_byte_index] = (counts & 255).try_into().unwrap_or(255);
+
+            match bulk.write(&measures) {
+                Ok(_) => (),
+                Err(_) => {
+                    serial.write(b"ERROR");
+                    ()
+                }
+            };
+        }
     }
 }
 
